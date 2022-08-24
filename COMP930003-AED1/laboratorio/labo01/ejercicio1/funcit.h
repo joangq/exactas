2223d3c133963c859633a10107bc244c60321c72
Joan Gonzalez <joanigq117@gmail.com>
2022-08-24 11:18:28 -0300

    Create funcit.h

---
+++ COMP930003-AED1/laboratorio/labo01/ejercicio1/funcit.h
@@ -0,0 +1,8 @@
+
+/* Implementación iterativa de las funciones del ejercicio 1 */
+
+int fibonacciIterativo(int n);
+int sumaImparesIterativa(int n);
+int sumaDivisoresIterativa(int n);
+int factorialIterativo(int n);
+int numeroCombinatorioIterativo(int n, int k);
