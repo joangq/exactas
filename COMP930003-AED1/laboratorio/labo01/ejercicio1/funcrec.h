ef2001f01900d9e035f5b84279acf8508c862c3e
Joan Gonzalez <joanigq117@gmail.com>
2022-08-24 11:18:42 -0300

    Create funcrec.h

---
+++ COMP930003-AED1/laboratorio/labo01/ejercicio1/funcrec.h
@@ -0,0 +1,8 @@
+/* Implementación recursiva de las funciones del ejercicio 1 */
+
+
+int fibonacciRecursivo(int n);
+int sumaImparesRecursiva(int n);
+int divisoresHasta(int num, int x);
+int sumaDivisoresRecursiva(int n);
+int numeroCombinatorioRecursivo(int n, int k)
